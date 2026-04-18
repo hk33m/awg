051a15ffc14a6fdc55006f5ed814c83dba716004
HK33M <alwrafy18@gmail.com>
2026-04-19 02:30:56 +0300

    first commit

---
+++ app/3d-animations/page.js
@@ -0,0 +1,7 @@
+'use client'
+
+import ModelViewer from './ModelViewer'
+
+export default function Page() {
+  return <ModelViewer url="/pros.glb" />
+}
--- app/layout.js
+++ app/layout.js
@@ -4,9 +4,32 @@ import { Analytics } from "@vercel/analytics/next";
 
 
 export const metadata = {
-  title: "تسويق رقمي    ",
+  title:" أوج ",
   description:
-    "لوحة تحكم ",
+    "أوج هي لغة برمجة جديدة تهدف إلى تبسيط عملية تطوير البرمجيات وجعلها أكثر متعة وإنتاجية. تم تصميم أوج لتكون سهلة التعلم والاستخدام، مع التركيز على توفير تجربة تطوير سلسة وممتعة للمبرمجين من جميع المستويات.",
+    icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      {
+        url: '/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+      {
+        url: '/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+    ],
+    apple: [
+      {
+        url: '/apple-touch-icon.png',
+        sizes: '180x180',
+      },
+    ],
+  },
+
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({ children }) {
@@ -20,8 +43,6 @@ export default function RootLayout({ children }) {
           disableTransitionOnChange
         >
           {children}
-         
-         
         </ThemeProvider>
       </body>
     </html>
