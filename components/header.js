a676e4370cacf694bcb1d77ad2266209f3d0f15e
HK33M <alwrafy18@gmail.com>
2026-04-19 02:51:49 +0300

    fix project

--- components/header.js
+++ components/header.js
@@ -44,7 +44,7 @@ export default function Header({title, description}) {
             
           </div>
  <div className="text-center">
-            <h1 className="text-3xl sm:text-4xl font-bold bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 bg-clip-text text-transparent mb-2">
+            <h1 className="text-xl  md:text-3xl font-bold bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 bg-clip-text text-transparent mb-2">
                {title}
             </h1>
             <p className="text-gray-600 text-sm sm:text-base"> {description}</p>
